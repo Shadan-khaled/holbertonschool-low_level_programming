ffd5b448e7fa902923123752b66f3122380cd2a8
Shadan-khaled <Shadankhaled75@gmail.com>
2025-09-02 17:20:48 +0000

    Add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,6 +18,13 @@ sign *= -1;
 else if (*s >= '0' && *s <= '9')
 {
 started = 1;
+if (result > (INT_MAX - (*s - '0')) / 10)
+{
+if (sign == 1)
+return (INT_MAX);
+else
+return (INT_MIN);
+}
 result = result * 10 + (*s - '0');
 }
 else if (started)
