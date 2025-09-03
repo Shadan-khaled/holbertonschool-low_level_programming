bd627c7ad5f82c6e79a820f15e3a310828f5de52
Shadan-khaled <Shadankhaled75@gmail.com>
2025-09-03 17:41:28 +0000

    Add

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -9,7 +9,12 @@
 int main(void)
 {
     char s[10] = "My School";
+    char str[] = "Look up!\n";
+    char *ptr;
 
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
     printf("%s\n", s);
     rev_string(s);
     printf("%s\n", s);
