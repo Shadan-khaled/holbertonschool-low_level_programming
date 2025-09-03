d9771c562525f70a3ba13b4028c0e6769aa0fdc3
Shadan-khaled <Shadankhaled75@gmail.com>
2025-09-03 17:32:52 +0000

    Add

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,4 +1,25 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (i != 0)
+            printf(", ");
+        printf("%d", a[i]);
+    }
+    printf("\n");
+}
 
 /**
  * main - check the code
@@ -7,10 +28,11 @@
  */
 int main(void)
 {
-    char *str;
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
     return (0);
 }
 
